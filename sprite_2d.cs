13e48ee73f87b6bc79ee29aab6aeaba396390865
Alexander Stuart <ahdisease@gmail.com>
2023-09-14 14:04:45 -0400

    tank controls tutorial in c# and GDScript

--- sprite_2d.cs
+++ sprite_2d.cs
@@ -7,9 +7,21 @@ public partial class sprite_2d : Sprite2D
 	private float _angularSpeed = Mathf.Pi;
 	
 	public override void _Process(double delta) {
-		Rotation += _angularSpeed * (float)delta;
+		float direction = 0;
+		if (Input.IsActionPressed("ui_left")) {
+			direction = -1;
+		}
+		if (Input.IsActionPressed("ui_right")) {
+			direction = 1;
+		}
 		
-		Vector2 velocity = Vector2.Up.Rotated(Rotation) * _speed;
+		Rotation += _angularSpeed * direction * (float)delta;
+		
+		Vector2 velocity = Vector2.Zero;
+		if (Input.IsActionPressed("ui_up")) {
+			velocity = Vector2.Up.Rotated(Rotation) * _speed;	
+		}
+		
 		Position += velocity * (float)delta;
 	}
 }
