16a9e19cd5f606af74c37cd57b91bfae80669cca
Alexander Stuart <ahdisease@gmail.com>
2023-09-17 23:34:54 -0400

    added HUD scene to Main. updated methods to hook up functionality.

--- dodge_the_creeps/Main.cs
+++ dodge_the_creeps/Main.cs
@@ -8,20 +8,20 @@ public partial class Main : Node
 	
 	private int _score;
 	
-	public override void _Ready()
-	{
-		NewGame();
-	}
-	
 	public void GameOver()
 	{
 		GetNode<Timer>("MobTimer").Stop();
 		GetNode<Timer>("ScoreTimer").Stop();
+		GetNode<HUD>("HUD").ShowGameOver();
 	}
 	
 	public void NewGame()
 	{
 		_score = 0;
+		
+		HUD hud = GetNode<HUD>("HUD");
+		hud.UpdateScore(_score);
+		hud.ShowMessage("Get Ready!");
 		
 		Player player = GetNode<Player>("Player");
 		Marker2D startPosition = GetNode<Marker2D>("StartPosition");
@@ -39,6 +39,7 @@ public partial class Main : Node
 	private void OnScoreTimerTimeout()
 	{
 		_score++;
+		GetNode<HUD>("HUD").UpdateScore(_score);
 	}
 	
 	private void OnMobTimerTimeout()
@@ -68,15 +69,3 @@ public partial class Main : Node
 		AddChild(mob);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
