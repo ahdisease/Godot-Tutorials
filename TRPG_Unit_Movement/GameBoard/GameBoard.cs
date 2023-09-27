0e56a0ed8ead405264d7e434abb25a1df500c359
Alexander Stuart <ahdisease@gmail.com>
2023-09-27 14:37:31 -0400

    added flood fill related methods. currently untested

--- TRPG_Unit_Movement/GameBoard/GameBoard.cs
+++ TRPG_Unit_Movement/GameBoard/GameBoard.cs
@@ -40,4 +40,69 @@ public partial class GameBoard : Node2D
             _units.Add(unit.Cell,unit);
         }
     }
+
+    public Vector2[] GetWalkableCells(Unit unit)
+    {
+        return _FloodFill(unit.Cell, unit.moveRange).ToArray();
+    }
+
+    private List<Vector2> _FloodFill(Vector2 cell, int maxDistance)
+    {
+        List<Vector2> fillRange = new List<Vector2>();
+
+        //create the stack of cells to check neighbors of for valid traversal
+        Stack<Vector2> toBeChecked = new Stack<Vector2>();
+        toBeChecked.Push(cell);
+        
+        //loop through until empty
+        while (toBeChecked.Count > 0)
+        {
+            Vector2 currentCell = toBeChecked.Pop();
+
+            if (!_grid.IsWithinBounds(currentCell))
+            {
+                continue;
+            }
+
+            if (fillRange.Contains(currentCell))
+            {
+                continue;
+            }
+
+            //ensure that cell is within range of unit's movement
+            Vector2 differenceBetweenCells = (currentCell - cell).Abs();
+            int distanceBetweenCells = Mathf.FloorToInt(differenceBetweenCells.X + differenceBetweenCells.Y);
+            if (distanceBetweenCells > maxDistance)
+            {
+                continue;
+            }
+
+            fillRange.Add(currentCell);
+
+            _AddNeighboringCellsToBeChecked(fillRange, toBeChecked, currentCell);
+        }
+
+
+        return fillRange;
+    }
+
+    private void _AddNeighboringCellsToBeChecked(List<Vector2> fillRange, Stack<Vector2> toBeChecked, Vector2 currentCell)
+    {
+        foreach (Vector2 direction in DIRECTIONS)
+        {
+            Vector2 neighboringCell = currentCell + direction;
+
+            if (IsOccupied(neighboringCell))
+            {
+                continue;
+            }
+
+            if (fillRange.Contains(neighboringCell))
+            {
+                continue;
+            }
+
+            toBeChecked.Push(neighboringCell);
+        }
+    }
 }
